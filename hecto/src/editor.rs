e8095fa0b6f296061d9067461919b9c515cd698d
qiang.zeng <qiang.zeng@iglooinsure.com>
2023-10-13 14:27:08 +0800

    hecto

---
+++ hecto/src/editor.rs
@@ -0,0 +1,54 @@
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn run(&self) {
+        let _stdout: termion::raw::RawTerminal<io::Stdout> = stdout().into_raw_mode().unwrap();
+        for key in io::stdin().keys() {
+            match key {
+                Ok(key) => match key {
+                    Key::Char(c) => {
+                        if c.is_control() {
+                            println!("{:?}\r", c as u8);
+                        } else {
+                            println!("{:?} ({})\r", c as u8, c);
+                        }
+                    }
+                    Key::Ctrl('q') => break,
+                    _ => println!("{:?}\r", key),
+                },
+                Err(err) => die(err),
+            }
+        }
+    }
+
+    // static function
+    pub fn default() -> Self {
+        Self {}
+    }
+
+    fn process_keypress(&self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => panic!("Program end"),
+            _ => (),
+        }
+        Ok(())
+    }
+}
+
+fn die(e: io::Error) {
+    panic!("{}", e);
+}
+
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
+    }
+}
